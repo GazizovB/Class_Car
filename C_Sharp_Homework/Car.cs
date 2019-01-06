068743e7b934189ae78d59e4fbb8e975fa3a2d05
Бауыржан <@->
2019-01-07 01:36:31 +0600

    Добавьте файлы проекта.

---
+++ C_Sharp_Homework/Car.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Homework
+{
+       public partial class Car
+    {
+        //Поля класса
+        public string _model;
+        private static int _cost;
+        private static double _mass;
+        public DateTime _date;
+        public int _mileage;
+
+        //Методы класса
+        public void SetDescribe(int cost)
+        {
+            _cost = cost;
+        }
+        public int GetDescribe()
+        {
+            return _cost;
+        }
+
+        //Перегруженный конструктор
+        public Car(string model, DateTime date, int mileage, int mass)
+        {
+            _model = model;
+            _date = date;
+            _mileage = mileage;
+            _mass = mass;
+        }
+
+        public string ShowCar() //Метод ShowCar
+        {
+            return $"Модель: {_model}\nСтоимость: {_cost}\nDate: {_date}\nПробег: {_mileage}\nMass: {_mass}"; 
+        }
+
+        //Конструктор без параметра
+        public Car() { }
+
+        //Метод PrintArray (показывает массив эл.)
+        public void PrintArray(int[] number)
+        {
+            Console.Write("Массив элементов: ");
+            for (int i = 0; i < number.Length; i++)
+            {
+                Console.Write(number[i]);
+            }
+        }
+
+    }
+}
---
+++ C_Sharp_Homework/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\t\t\tВыберите машины, например \"1\"");
+            Console.WriteLine("1. Mercedes Benz");
+            Console.WriteLine("2. Hyundai");
+            Console.WriteLine("3. Toyota");
+            Console.WriteLine();
+
+            int Choise1 = int.Parse(Console.ReadLine());
+
+            //Выбор машины и отображение
+            if (Choise1 == 1)
+            {
+                Car MercedesBenz = new Car("Mercedes Benz", new DateTime(2015, 01, 03), 12345, 432);
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine(MercedesBenz.ShowCar());
+            }
+            else if (Choise1 == 2)
+            {
+                Car Hyundai = new Car("Hyundai", new DateTime(2016, 12, 05), 34543, 324);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(Hyundai.ShowCar());
+            }
+            else if (Choise1 == 3)
+            {
+                Car Toyota = new Car("Toyota", new DateTime(2018, 11, 07), 34543, 410);
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine(Toyota.ShowCar());
+            }
+
+
+            Car massiv = new Car();
+            int[] arr = { 2, 3, 4, 4, 4 };
+
+            massiv.PrintArray(arr);
+
+
+            Console.ReadLine();
+        }
+    }
+}
